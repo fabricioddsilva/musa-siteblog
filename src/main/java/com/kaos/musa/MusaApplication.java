c06efdce37cb3e526488a5a1cf77a7cc2294485d
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-11-05 13:59:35 -0300

    Criação do Projeto

---
+++ src/main/java/com/kaos/musa/MusaApplication.java
@@ -0,0 +1,13 @@
+package com.kaos.musa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusaApplication.class, args);
+	}
+
+}
